7ebc11149920c5e5932e25df1cb9aea163dde3ba
Sreenadh T C <sreenadh@datahut.co>
2018-04-02 21:29:44 +0530

    Fix profile section, Add posts component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
     :mini-variant="miniVariant"
     :clipped="clipped"
     v-model="drawer"
-    mini-variant-width="100"
+    mini-variant-width="150"
     width="450"
     enable-resize-watcher
     fixed
@@ -20,7 +20,7 @@
               <v-avatar
               :tile="false"
 
-              :size="miniVariant ? 50 : 250"
+              :size="miniVariant ? 90 : 250"
               class="grey lighten-4"
               style="cursor:pointer"
               >
@@ -31,38 +31,51 @@
               <div class="subheading text-xs-center grey--text pt-1 pb-3">Product Engineer | Hashwave Technologies</div>
             </div>
           </div>
-          </v-flex>
-          <v-divider></v-divider>
-          <v-layout justify-space-between v-if="!miniVariant">
+          <v-divider dark></v-divider>
+        </v-flex>
+        <v-layout justify-space-between v-if="!miniVariant">
 
-            <v-btn flat icon href="#"><v-icon mini>fa-dashboard</v-icon></v-btn>
-            <v-btn flat icon href="#"><v-icon mini>fa-twitter-square</v-icon></v-btn>
-            <v-btn flat icon href="#"><v-icon mini>fa-github</v-icon></v-btn>
-            <v-btn flat icon href="#"><v-icon mini>fa-facebook</v-icon></v-btn>
-          </v-layout>
-          <v-layout row wrap align-center v-if="miniVariant">
-            <v-flex xs12 sm12 md12 lg12 class="text-xs-center">
-              <v-btn icon href="#"><v-icon medium>fa-at</v-icon></v-btn>
-            </v-flex>
-            <v-flex xs12 sm12 md12 lg12 class="text-xs-center">
-              <v-btn icon href="#"><v-icon medium>fa-twitter</v-icon></v-btn>
-            </v-flex>
-            <v-flex xs12 sm12 md12 lg12 class="text-xs-center">
-              <v-btn icon href="#"><v-icon medium>fa-github</v-icon></v-btn>
-            </v-flex>
-            <v-flex xs12 sm12 md12 lg12 class="text-xs-center">
-              <v-btn icon href="#"><v-icon medium>fa-facebook</v-icon></v-btn>
-            </v-flex>
-          </v-layout>
+          <v-btn flat icon href="#"><v-icon size="1.5rem">fa-at</v-icon></v-btn>
+          <v-btn flat icon href="#"><v-icon size="1.5rem">fa-twitter-square</v-icon></v-btn>
+          <v-btn flat icon href="#"><v-icon size="1.5rem">fa-github</v-icon></v-btn>
+          <v-btn flat icon href="#"><v-icon size="1.5rem">fa-facebook</v-icon></v-btn>
+        </v-layout>
+        <v-layout row wrap align-center v-if="miniVariant">
+          <v-flex xs12 sm12 md12 lg12 class="text-xs-center">
+            <v-btn icon href="#"><v-icon medium>fa-at</v-icon></v-btn>
+          </v-flex>
+          <v-flex xs12 sm12 md12 lg12 class="text-xs-center">
+            <v-btn icon href="#"><v-icon medium>fa-twitter</v-icon></v-btn>
+          </v-flex>
+          <v-flex xs12 sm12 md12 lg12 class="text-xs-center">
+            <v-btn icon href="#"><v-icon medium>fa-github</v-icon></v-btn>
+          </v-flex>
+          <v-flex xs12 sm12 md12 lg12 class="text-xs-center">
+            <v-btn icon href="#"><v-icon medium>fa-facebook</v-icon></v-btn>
+          </v-flex>
+        </v-layout>
       </v-layout>
+      <v-divider dark></v-divider>
     </v-container>
   </v-toolbar>
 </v-navigation-drawer>
 <v-content>
   <router-view/>
 </v-content>
-<v-footer :fixed="fixed" app>
-  <span>&copy; 2017</span>
+<v-footer color="teal darken-2" app>
+  <v-layout row wrap align-center>
+    <v-flex xs10 sm10 md11 lg11>
+      <div class="white--text ml-3 text-xs-center">
+        Made with
+        <v-icon mini class="white--text">favorite</v-icon> by Sreenadh TC
+      </div>
+    </v-flex>
+    <v-flex xs2 sm2 md1 lg1>
+        <v-btn icon flat :href="'https://github.com/sreecodeslayer/Flogger'">
+          <v-icon class="white--text" style="cursor:pointer;" medium>fa-github</v-icon>
+        </v-btn>
+    </v-flex>
+  </v-layout>
 </v-footer>
 </v-app>
 </template>
@@ -74,14 +87,8 @@ export default {
       clipped: false,
       drawer: true,
       fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
       miniVariant: true,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Sreenadh TC - Flogger'
     }
   },
   name: 'App'
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout row wrap>
+        <v-flex xs9 sm9 md9 lg9>
+          <v-layout row wrap>
+            <v-flex xs12 v-for="post in posts" :key="post.id">
+              <v-card class="my-3" hover>
+                <v-card-media class="white--text" height="150px" :src="'http://via.placeholder.com/150x50'">
+                  <v-container fill-height fluid>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end felxbox>
+                        <span class="headline">{{post.title}}</span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-media>
+                <v-card-title>
+                  <div>
+                    <span class="grey--text">{{post.created_on}}</span><br>
+                  </div>
+                </v-card-title>
+                <v-card-text>
+                  {{post.meta}}
+                </v-card-text>
+                <v-card-actions>
+                 <v-btn icon class="red--text">
+                  <v-icon mini>fa-heart</v-icon>
+                </v-btn>
+                <v-btn icon class="blue--text">
+                  <v-icon mini>fa-share</v-icon>
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn small flat class="teal--text" :href="post.url">Read</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-slide-y-transition>
+</v-container>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  methods: {
+    getPosts () {
+
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
