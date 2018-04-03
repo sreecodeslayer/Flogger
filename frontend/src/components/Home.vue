526256c2a9cbd8de34fe42824f9d8d7e3d6723a5
Sreenadh TC <kesav.tc8@gmail.com>
2018-04-03 17:01:26 +0530

    Add pagination, revamp ui with loaders

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -3,73 +3,82 @@
     <v-slide-y-transition mode="out-in">
       <v-layout row wrap>
         <v-flex xs9 sm9 md9 lg9 offset-md1 offset-lg2>
-          <v-layout row wrap v-if="loaded">
-            <v-flex xs12 v-for="post in posts" :key="post.id" v-cloak>
-              <v-card class="my-3" hover>
-                <v-parallax :src="post.cover_img" v-ripple height="450">
-                  <v-card-media class="white--text" v-ripple height="450">
-                    <v-container fill-height fluid>
-                      <v-layout fill-height>
-                        <v-flex xs12 align-end felxbox>
-                          <span class="headline">{{post.title}}</span>
-                        </v-flex>
-                      </v-layout>
-                    </v-container>
-                  </v-card-media>
-                </v-parallax>
-                <v-card-title>
-                  <div>
-                    <v-tooltip bottom>
-                      <span slot="activator" class="grey--text">{{post.created_on.$date | humanizeTime}}</span>
-                      <span>{{post.created_on.$date | calendarTime}}</span>
-                    </v-tooltip>
-                  </div>
-                </v-card-title>
-                <v-card-text>
-                  {{post.caption}}
-                </v-card-text>
-                <v-card-actions>
-                 <v-btn icon class="red--text">
-                  <v-icon mini>fa-heart</v-icon>
-                </v-btn>
-                <v-bottom-sheet v-model="shareSheet">
-                  <v-btn slot="activator" icon class="blue--text" @click.native="postToShare = post">
-                    <v-icon mini>fa-share</v-icon>
+          <div v-if="loaded">
+            <v-layout row wrap v-if="loaded">
+              <v-flex xs12 v-for="post in posts" :key="post.id" v-cloak>
+                <v-card class="my-3" hover>
+                  <v-parallax :src="post.cover_img" v-ripple height="450">
+                    <v-card-media class="white--text" v-ripple height="450">
+                      <v-container fill-height fluid>
+                        <v-layout fill-height>
+                          <v-flex xs12 align-end felxbox>
+                            <span class="headline">{{post.title}}</span>
+                          </v-flex>
+                        </v-layout>
+                      </v-container>
+                    </v-card-media>
+                  </v-parallax>
+                  <v-card-title>
+                    <div>
+                      <v-tooltip bottom>
+                        <span slot="activator" class="grey--text">{{post.created_on.$date | humanizeTime}}</span>
+                        <span>{{post.created_on.$date | calendarTime}}</span>
+                      </v-tooltip>
+                    </div>
+                  </v-card-title>
+                  <v-card-text>
+                    {{post.caption}}
+                  </v-card-text>
+                  <v-card-actions>
+                   <v-btn icon class="red--text">
+                    <v-icon mini>fa-heart</v-icon>
                   </v-btn>
-                  <v-list>
-                    <v-subheader>Share post via</v-subheader>
-                    <v-list-tile color="indigo darken-1" @click="shareSheet = false;sharePost()">
-                      <v-list-tile-avatar>
-                        <v-icon medium color="indigo darken-1">fa-facebook</v-icon>
-                      </v-list-tile-avatar>
-                      <v-list-tile-content>Facebook</v-list-tile-content>
-                    </v-list-tile>
-                    <v-list-tile color="blue lighten-2" @click="shareSheet = false;sharePost()">
-                      <v-list-tile-avatar>
-                        <v-icon medium color="blue lighten-2">fa-twitter</v-icon>
-                      </v-list-tile-avatar>
-                      <v-list-tile-content>Twitter</v-list-tile-content>
-                    </v-list-tile>
-                    <v-list-tile color="teal darken-3" @click="shareSheet = false;sharePost()">
-                      <v-list-tile-avatar>
-                        <v-icon medium color="teal darken-3">fa-whatsapp</v-icon>
-                      </v-list-tile-avatar>
-                      <v-list-tile-content>WhatsApp</v-list-tile-content>
-                    </v-list-tile>
-                    <v-list-tile color="lime darken-3" @click="shareSheet = false;sharePost()">
-                      <v-list-tile-avatar>
-                        <v-icon medium color="lime darken-3">fa-copy</v-icon>
-                      </v-list-tile-avatar>
-                      <v-list-tile-content>Clipboard</v-list-tile-content>
-                    </v-list-tile>
-                  </v-list>
-                </v-bottom-sheet>
-                <v-spacer></v-spacer>
-                <v-btn small flat class="teal--text" :href="'#/posts/'+post._id.$oid">Read</v-btn>
-              </v-card-actions>
-            </v-card>
+                  <v-bottom-sheet v-model="shareSheet">
+                    <v-btn slot="activator" icon class="blue--text" @click.native="postToShare = post">
+                      <v-icon mini>fa-share</v-icon>
+                    </v-btn>
+                    <v-list>
+                      <v-subheader>Share post via</v-subheader>
+                      <v-list-tile color="indigo darken-1" @click="shareSheet = false;sharePost()">
+                        <v-list-tile-avatar>
+                          <v-icon medium color="indigo darken-1">fa-facebook</v-icon>
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>Facebook</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile color="blue lighten-2" @click="shareSheet = false;sharePost()">
+                        <v-list-tile-avatar>
+                          <v-icon medium color="blue lighten-2">fa-twitter</v-icon>
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>Twitter</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile color="teal darken-3" @click="shareSheet = false;sharePost()">
+                        <v-list-tile-avatar>
+                          <v-icon medium color="teal darken-3">fa-whatsapp</v-icon>
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>WhatsApp</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile color="lime darken-3" @click="shareSheet = false;sharePost()">
+                        <v-list-tile-avatar>
+                          <v-icon medium color="lime darken-3">fa-copy</v-icon>
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>Clipboard</v-list-tile-content>
+                      </v-list-tile>
+                    </v-list>
+                  </v-bottom-sheet>
+                  <v-spacer></v-spacer>
+                  <v-btn small flat class="teal--text" :href="'#/posts/'+post._id.$oid">Read</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+          <v-layout row wrap align-center>
+            <v-flex xs12>
+            <div class="text-xs-center" v-cloak>
+              <v-pagination :length="totalPages" value="page" v-model="page"></v-pagination>
+            </div>
           </v-flex>
-        </v-layout>
+          </v-layout>
+        </div>
         <!-- <v-layout row wrap align-center v-else> -->
           <div class="topcorner-loader" v-else>
             <v-spinner :animation-duration="2000"
@@ -91,13 +100,18 @@ export default {
       posts: [],
       shareSheet: false,
       postToShare: null,
-      loaded: false
+      loaded: false,
+      page: 1,
+      perPage: 1,
+      totalPages: 0
     }
   },
   methods: {
     getPosts () {
-      var url = 'http://127.0.0.1:8000/api/posts'
+      var url = 'http://127.0.0.1:8000/api/posts?page=' + this.page + '&per_page=' + this.perPage
       this.$http.get(url).then((response) => {
+        var posts = response.data.posts
+        this.totalPages = parseInt(posts.total / posts.per_page)
         this.posts = response.data.posts.items
         this.loaded = true
       })
--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,12 +1,38 @@
 <template>
 	<v-container fluid>
 		<v-slide-y-transition mode="out-in">
-			<v-layout row wrap v-if="loaded">
-        <v-flex xs10 offset-xs2 class="article" v-html="post.content">
-        </v-flex>
-			</v-layout>
-		</v-slide-y-transition>
-	</v-container>
+			<div v-if="loaded" class="article">
+        <v-layout row wrap>
+          <v-flex xs10 offset-xs2>
+            <h1 class="display-2 teal--text darken-2" >{{post.title}}</h1>
+            <br>
+            <v-layout row wrap>
+              <v-flex xs10>
+                <div class="subheading">{{post.caption}}</div>
+              </v-flex>
+              <v-flex xs2  class="teal--text darken-2" v-ripple>
+                <span class="caption">
+                  <v-icon color="teal darken-2" mini flat>fa-calendar</v-icon>
+                  {{ post.created_on.$date | calendarTime }}
+                </span>
+              </v-flex>
+            </v-layout>
+            <v-divider></v-divider>
+            <br>
+          </v-flex>
+          <v-flex xs10 offset-xs2 v-html="post.content">
+          </v-flex>
+        </v-layout>
+      </div>
+      <div class="topcorner-loader" v-else>
+        <v-spinner :animation-duration="2000"
+        :size="50"
+        color="#ff1d5e"
+        ></v-spinner>
+      </div>
+
+    </v-slide-y-transition>
+  </v-container>
 </template>
 <script>
 export default {
@@ -28,8 +54,9 @@ export default {
           this.post = response.data.post
           this.loaded = true
         },
-        (response) => {
-
+        (err) => {
+          var errData = err.response
+          this.$router.push('/oops/' + errData.status)
         }
         )
     }
